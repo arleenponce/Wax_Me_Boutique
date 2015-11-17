26d695c37e5369a2a9b29c6e93276b2576439c4a
Arleen Ponce <arleen.ponce@gmail.com>
2015-11-16 20:55:09 -0800

    Got initial scaffolding done. Began landing page design with Solarized template.

---
+++ public/homepage/js/banner_images.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+var scroller = $('#scroller div.innerScrollArea');
+        var scrollerContent = scroller.children('ul');
+        scrollerContent.children().clone().appendTo(scrollerContent);
+        var curX = 0;
+        scrollerContent.children().each(function(){
+            var $this = $(this);
+            $this.css('left', curX);
+            curX += $this.outerWidth(true);
+        });
+        var fullW = curX / 2;
+        var viewportW = scroller.width();
+
+        // scroller.css('overflow-x', 'auto');
+
+});
