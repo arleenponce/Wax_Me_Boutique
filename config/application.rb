2a2cdae7170bec81e4f46676cf775878a8df698b
Arleen Ponce <arleen.ponce@gmail.com>
2016-01-20 10:41:40 -0800

    Edit production environment

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module WaxMeBoutique
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_files = true
+    config.assets.compile = true
+    config.active_record.raise_in_transactional_callbacks = truea
   end
 end
