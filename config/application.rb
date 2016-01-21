56b1fb6c01295a7c72ab911a090166fdc1cf4c90
Arleen Ponce <arleen.ponce@gmail.com>
2016-01-20 17:18:37 -0800

    Application error.

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module WaxMeBoutique
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    # config.serve_static_assets = true
     config.active_record.raise_in_transactional_callbacks = truea
   end
 end
