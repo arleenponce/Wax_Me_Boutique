fc6e551346497002dd6d72ac67d12444ef1b3074
Arleen Ponce <arleen.ponce@gmail.com>
2016-01-25 10:09:22 -0800

    Remove unecessary javascript from Google API JS

--- public/js/google_maps.js
+++ public/js/google_maps.js
@@ -25,25 +25,5 @@ $(document).ready(function(){
       scrollwheel: false,
       zoom: 7
     });
-
-    var directionsDisplay = new google.maps.DirectionsRenderer({
-      map: map
-    });
-
-    // Set destination, origin and travel mode.
-    var request = {
-      destination: waxmebotique,
-      origin: home,
-      travelMode: google.maps.TravelMode.DRIVING
-    };
-
-    // Pass the directions request to the directions service.
-    var directionsService = new google.maps.DirectionsService();
-    directionsService.route(request, function(response, status) {
-      if (status == google.maps.DirectionsStatus.OK) {
-        // Display the route on the map.
-        directionsDisplay.setDirections(response);
-      }
-    });
   }
 });
