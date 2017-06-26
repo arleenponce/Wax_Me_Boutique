775ce923924305acbf808c07a1e8907e68a6cab8
Arleen Ponce <arleen.ponce@gmail.com>
2017-06-25 20:03:34 -0700

    latest changes

--- public/js/pull_instagram.js
+++ public/js/pull_instagram.js
@@ -1,21 +1,10 @@
 $(document).ready(function() {
-  // var feed = new Instafeed({
-  //       target: 'others_instafeed',
-  //       get: 'tagged',
-  //       tagName: 'waxmeboutique',
-  //       clientId: '4798865111324c578fe9738bcbcf1515',
-  //       accessToken: '1274887324.1677ed0.5942c550d18346d4b7aa58b43bab89c6'
-  //       accessToken: '19580610.1677ed0.6329a93d992842bea67e3dbd8c804ec9',
-  //       limit: '5',
-  //       template: '<a href="{{link}}"><img src="{{image}}"/></a><br/><label><b></b>: {{caption}}</label>'
-  //   });
-  //   feed.run();
 
-  var feed2 = new Instafeed({
+  	var feed2 = new Instafeed({
         target: 'our_instafeed',
         get: 'user',
         clientId: '4798865111324c578fe9738bcbcf1515',
-        accessToken: '19580610.4798865.e026e967744d4ee797d7605256675fbf',
+        accessToken: '19580610.4798865.9f1ea8ae93ab4c108798aa9a3897180f',
         userId: '1274887324',
         limit: '5',
     });
